12bd2d2b07c79112988baea93e89ddc016072dd5
Md Hedayet Ullah <tanmoy15-1004@diu.edu.bd>
2023-06-13 18:27:16 +0600

    side bar updated.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,7 @@ const SideBar = () => {
     return (
         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                         <Link
-                        to="/dashboard"
+                        to="/dashboard/todo"
                         className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none text-center"
                         >
                         <span className="fs-5 d-none d-sm-inline text-center">Todo App</span>
